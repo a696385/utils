5c1dc46e56124b4a0fc3421cf0c0f06cc7bc886f
andy <sumskoy@realweb.ru>
2013-07-12 11:23:33 +0400

    upd

--- stackworker.js
+++ stackworker.js
@@ -5,7 +5,7 @@
  */
 
 var timers = require('timers'),
-	utils = require('../utils');
+    utils = require('../utils');
 
 // Set processor, setImmediate if 0.10 otherwise nextTick
 var processor = timers.setImmediate ? timers.setImmediate : process.nextTick;
@@ -16,12 +16,12 @@ var processor = timers.setImmediate ? timers.setImmediate : process.nextTick;
  * @constructor
  */
 var StackWorker = function(interval, canDoJob){
-	if (interval == null) interval = 1000;
-	if (canDoJob == null) canDoJob = function(){ return true; };
-	this.interval = interval;
-	this.canDoJob = canDoJob;
-	this.jobs = [];
-	this.worked = false;
+    if (interval == null) interval = 1000;
+    if (canDoJob == null) canDoJob = function(){ return true; };
+    this.interval = interval;
+    this.canDoJob = canDoJob;
+    this.jobs = [];
+    this.worked = false;
 };
 
 /**
@@ -31,61 +31,61 @@ var StackWorker = function(interval, canDoJob){
  * @param {Array} args Arguments for function call
  */
 StackWorker.prototype.addJob = function(owner, func, args){
-	this.jobs.push({
-		owner: owner,
-		f: func,
-		args: args
-	});
-	this.start();
+    this.jobs.push({
+        owner: owner,
+        f: func,
+        args: args
+    });
+    this.start();
 };
 
 /**
  * Start execute all jobs
  */
 StackWorker.prototype.start = function(){
-	var self = this;
-	if (self.worked) return;
-	if (!self.canDoJob()) return;
-	self.worked = true;
-	var iterate = function(isFirst){
-		var job;
-		var first = self.jobs.splice(0, 1);
-		if (first.length > 0){
-			job = first[0];
-		}
-		if (!job){
-			self.worked = false;
-			return;
-		}
-		var onTimeOut = function(){
-			var originalCallback = job.args.pop();
-			if (utils.isFunction(originalCallback)){
-				var fakeCallback = function(){
-					var args = Array.prototype.slice.call(arguments);
-					originalCallback.apply(this, args);
-					iterate();
-				};
-				job.args.push(fakeCallback);
-			} else {
-				if (originalCallback) job.args.push(originalCallback);
-				var fakeCallback = function(){
-					iterate();
-				};
-				job.args.push(fakeCallback);
-			}
-			job.f.apply(job.owner, job.args);
-		};
-		if (!isFirst){
-			if (self.interval > 0){
-				setTimeout(onTimeOut, self.interval);
-			} else {
-				processor(onTimeOut);
-			}
-		} else {
-			onTimeOut();
-		}
-	};
-	iterate(true);
+    var self = this;
+    if (self.worked) return;
+    if (!self.canDoJob()) return;
+    self.worked = true;
+    var iterate = function(isFirst){
+        var job;
+        var first = self.jobs.splice(0, 1);
+        if (first.length > 0){
+            job = first[0];
+        }
+        if (!job){
+            self.worked = false;
+            return;
+        }
+        var onTimeOut = function(){
+            var originalCallback = job.args.pop();
+            if (utils.isFunction(originalCallback)){
+                var fakeCallback = function(){
+                    var args = Array.prototype.slice.call(arguments);
+                    originalCallback.apply(this, args);
+                    iterate();
+                };
+                job.args.push(fakeCallback);
+            } else {
+                if (originalCallback) job.args.push(originalCallback);
+                var fakeCallback = function(){
+                    iterate();
+                };
+                job.args.push(fakeCallback);
+            }
+            job.f.apply(job.owner, job.args);
+        };
+        if (!isFirst){
+            if (self.interval > 0){
+                setTimeout(onTimeOut, self.interval);
+            } else {
+                processor(onTimeOut);
+            }
+        } else {
+            onTimeOut();
+        }
+    };
+    iterate(true);
 };
 
 module.exports = StackWorker;
